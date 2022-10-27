08563411b92589d691041be13b2e6401b274518f
NicolasOntaneda <nicolas.ontaneda@trade.ec>
2022-10-27 08:26:29 -0500

    firebase added

---
+++ src/firebase-conf.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCk4EQ9o3cJz1IP6mM__yVuOUPDX0C3LUk",
+  authDomain: "micorep1.firebaseapp.com",
+  projectId: "micorep1",
+  storageBucket: "micorep1.appspot.com",
+  messagingSenderId: "283740629163",
+  appId: "1:283740629163:web:dab0c592cc2076835fefee"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
